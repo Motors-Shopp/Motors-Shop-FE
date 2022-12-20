e55ddc33a92fc0613010540f0931bba9ed7aa4b4
Bruno Gomes <gomesbruno030@outlook.com>
2022-12-20 18:20:39 -0300

    feat-navBar add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import VehicleCard from "./components/vehicleCard/index"
 import Footer from "./components/footers/footer";
 import Header from "./components/header/index";
+import NavBar from "./components/navBar/index";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <NavBar></NavBar>
       <main>
         <Footer />
       </main>
