ed0fe7e27519cb79f52378ca23a21d2fa40d7d59
anjosdelacerda <anjosdelacerda@gmail.com>
2022-12-22 02:50:01 -0300

    STYLE: Sem alterações

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 // import VehicleCard from "./components/vehicleCard/index"
-import Footer from "./components/footers/footer";
-import Header from "./components/header/index";
+// import Footer from "./components/footers/footer";
+// import Header from "./components/header/index";
+import Announcement from "./pages/announcements/Announcement";
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <main>
-        <Footer />
-      </main>
-    </div>
+    <>
+      <Announcement />
+    </>
   );
 }
 
