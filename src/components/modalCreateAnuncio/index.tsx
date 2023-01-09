7a93786f922afebd2374de414097dca2b50d1a15
Bruno Gomes <gomesbruno030@outlook.com>
2023-01-09 13:39:22 -0300

    fix-css-modalOfCreateAnuncio

--- src/components/modalCreateAnuncio/index.tsx
+++ src/components/modalCreateAnuncio/index.tsx
@@ -11,33 +11,33 @@ function modalCreateAnuncio({ setTipoVeicle,setModal,handleSubmit,onSubmitFuncti
         <button className="btnModal" type="submit"></button>
        */}
       <div className="barCriarAnuncio">
-            <p className="textAjusting1">Criar anuncio</p>
+            <p className="textAjusting2">Criar anuncio</p>
             <div
             onClick={() => {
                 setModal(false);
             }}
             className="closeModal"
             >
-            <p>X</p>
+            <p className="BtnX">X</p>
             </div>
         </div>
         <div className="tipoAnuncio">
-            <p className="textAjusting1">tipo de anuncio</p>
+            <p className="textAjusting2">tipo de anuncio</p>
         </div>
         <div className="conteinerBtnModal">
             <button className="btnModal">Venda</button>
             <button className="btnModal">Leilão</button>
         </div>
-        <p className="textAjusting1">informações do veiculo</p>
+        <p className="textAjusting2">informações do veiculo</p>
         <p className="textAjusting1">Titulo</p>
         <input {...register("Titulo")} placeholder="titulo" className="inputModal1" type="text" />
         <div className="anoKMPreco">
             <div>
-            <p>Ano</p>
+            <p className="textAjusting3">Ano</p>
             <input {...register("Ano")} placeholder="Ano" className="inputModal2" type="text" />
             </div>
             <div>
-            <p>Quilometragem</p>
+            <p className="textAjusting3">Quilometragem</p>
             <input
                 placeholder="Quilometragem"
                 className="inputModal2"
@@ -46,23 +46,23 @@ function modalCreateAnuncio({ setTipoVeicle,setModal,handleSubmit,onSubmitFuncti
             />
             </div>
             <div>
-            <p>Preço</p>
+            <p className="textAjusting3">Preço</p>
             <input {...register("Preço")} placeholder="Preço" className="inputModal2" type="text" />
             </div>
         </div>
         <p className="textAjusting1">descrisção</p>
         <input {...register("descrisção")} placeholder="descrisção" className="inputModal3" type="text" />
-        <p className="textAjusting1">tipo de veiculo</p>
-        <div className="conteinerBtnModal">
+        <p className="textAjusting4">tipo de veiculo</p>
+        <div className="conteinerBtnModal2">
             <button onClick={()=>{setTipoVeicle("Carro")}} className="btnModal">Carro</button>
             <button onClick={()=>{setTipoVeicle("Moto")}} className="btnModal">Moto</button>
         </div>
         <p className="textAjusting1">imagem da capa</p>
-        <input {...register("imagemDaCapa")} placeholder="imagem da capa" className="inputModal1" type="text" />
+        <input {...register("imagemDaCapa")} placeholder="imagem da capa" className="inputModal4" type="text" />
         <p className="textAjusting1">1* imagem da galeria</p>
         <input
             placeholder="1* imagem da galeria"
-            className="inputModal1"
+            className="inputModal4"
             type="text"
             {...register("ImagemDaGaleria1")}
         />
